2c9ce4c73cf188574ef35c9a23e6883c4441612d
Egoitz <egoitttzzz@gmail.com>
2026-02-23 12:43:01 +0100

    Añadidos metodos a la clase de Autores

--- Autores.java
+++ Autores.java
@@ -29,4 +29,33 @@ class Autor {
     public String getapel2(){
         return this.apellido2;
     }
+     public void setcodigo(int pcodigoAutor){
+        this.codigoAutor=pcodigoAutor;
+    }
+    public void setnombre(String pnombre){
+        this.nombre=pnombre;
+    }
+    public void setapellido1(String papellido1){
+        this.apellido1=papellido1;
+    }
+    public void setapellido2(String papellido2){
+       this.apellido2=papellido2;
+    }
+    public void setnacimiento(int pnacimiento){
+        this.nacimiento=pnacimiento;
+    }
+    public void setlibros(Libros plibro){
+        this.libro=plibro;
+    }
+
+
+
+
+    public String toString() {
+        return "PRUEBA [codigoAutor=" + codigoAutor + ", nombre=" + nombre + ", apellido1=" + apellido1 + ", apellido2="
+                + apellido2 + ", nacimiento=" + nacimiento + "]";
+    }
+
+
+   
 }
